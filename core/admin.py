c1cbe59cc38a81c2011e767e2c6b1fda9b8cd634
Glauco Junior <gjunior@clb.santillana.com>
2024-02-20 17:33:24 -0300

    feat: Registered the models so the users can manage them from Django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Action, ActionType, Order, PaymentOrder, Product, Rule
+
+admin.site.register([Action, ActionType, Order, PaymentOrder, Product, Rule])
