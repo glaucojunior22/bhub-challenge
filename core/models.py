9b185ddfd91f0e4f3b4e3432421a455889cd0c48
Glauco Junior <gjunior@clb.santillana.com>
2024-02-20 11:52:36 -0300

    feat: Adds the basic models and its migrations to the core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+
+class PaymentOrder(models.Model):
+    PAYMENT_METHODS = (
+        ('credit_card', 'Credit Card'),
+        ('debit_card', 'Debit Card'),
+        ('bank_transfer', 'Bank Transfer'),
+        ('cash', 'Cash'),
+    )
+    PAYMENT_STATUSES = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=100, choices=PAYMENT_METHODS)
+    payment_status = models.CharField(max_length=100, choices=PAYMENT_STATUSES)
+
+    def __str__(self):
+        return f'Payment for {self.order}'
+
+
+class ActionType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Action(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    action_type = models.ForeignKey(ActionType, on_delete=models.CASCADE)
+    configuration = models.JSONField()
+
+    def __str__(self):
+        return self.name
+
+
+class Rule(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    actions = models.ManyToManyField(Action)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='rules',
+        related_query_name='rule',
+    )
+
+    def __str__(self):
+        return self.name
